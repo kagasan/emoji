d7554a903615de44bdf3b40aeb3d9bbaff5f430b
kagasan <ghghkaja@gmail.com>
2018-03-25 14:07:33 +0900

    保存しやすくした
    表示をcanvasから画像に変えたので保存しやすくなったはず

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var img = new Image();
 var imgFlg = 0;
 
 window.onload=function(){
-    canvas = document.getElementById('canvas');
+    canvas = document.createElement('canvas');
     ctx = canvas.getContext('2d');
     canvas.height=128;
     canvas.width=128;
@@ -34,10 +34,12 @@ window.onload=function(){
         ctx.font = "128px Unknown Font";
         ctx.fillStyle = "rgb(0, 0, 0)";
         ctx.fillText(curStr,4,110,120);
+        document.getElementById("output").innerHTML = "<img src='" + canvas.toDataURL() + "'></br>";
     }
     setInterval(loop,1000);
 }
 
 function fromImg(){
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    document.getElementById("output").innerHTML = "<img src='" + canvas.toDataURL() + "'></br>";
 }
