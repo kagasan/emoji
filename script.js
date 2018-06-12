85fce52cc10776eff137583ad3eb68a5886b3c4c
kagasan <ghghkaja@gmail.com>
2018-06-12 10:26:08 +0900

    two rows

--- script.js
+++ script.js
@@ -25,15 +25,26 @@ window.onload=function(){
         }
     }, false);
     var lastStr = "";
+    var lastStr2 = "";
     function loop(){
         var curStr = document.forms.src.str.value;
-        if(curStr==lastStr)return;
+        var curStr2 = document.forms.src.str2.value;
+        if(curStr==lastStr && curStr2==lastStr2)return;
         lastStr=curStr;
+        lastStr2=curStr2;
         ctx.fillStyle = "rgb(255, 255, 255)";
         ctx.fillRect(0, 0, 129, 129);
-        ctx.font = "128px Unknown Font";
-        ctx.fillStyle = "rgb(0, 0, 0)";
-        ctx.fillText(curStr,4,110,120);
+        if(lastStr2==""){
+            ctx.font = "128px Unknown Font";
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.fillText(curStr,4,110,120);
+        }
+        else{
+            ctx.font = "62px Unknown Font";
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.fillText(curStr,4,57,120);
+            ctx.fillText(curStr2,4,112,120);
+        }
         document.getElementById("output").innerHTML = "<img src='" + canvas.toDataURL() + "'></br>";
     }
     setInterval(loop,1000);
